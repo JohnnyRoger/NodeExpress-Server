c9d4f4e4124673c4035c24a8b2d25a0f8616920f
jrdemadara <jrdemadara@protonmail.com>
2025-06-29 12:24:15 +0800

    add authentication feature

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const createError = require('http-errors')
-const WebRoutes = require('./routes/web')
-const { connect } = require('./config/database')
-const cors = require('cors')
-const logger = require('./utils/logger');
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+const WebRoutes = require("./routes/web");
+const { connect } = require("./config/database");
+const cors = require("cors");
+const logger = require("./utils/logger");
+require("dotenv").config();
+const authController = require("./authController");
 
 //* express app
-const app = express()
-const port = process.env.APP_PORT
-app.use(bodyParser.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+const port = process.env.APP_PORT;
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// routes
+app.post("/register", authController.register);
+app.post("/login", authController.login);
+
+// start
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
